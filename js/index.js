5acb1d7d8b629b9b4083140cacca4c7339d4c04a
Frank Teng <Frank@Franks-Air.wv.cc.cmu.edu>
2017-12-08 23:14:30 -0500

    erased info

--- js/index.js
+++ js/index.js
@@ -1,18 +1,4 @@
-
-  /* TODO: Format a movie quote or any inspirational quote within a canvas.
-    Alternatively try recreating the design in the slide >:D */
 function setup() {
-  createCanvas(500, 500);
-  background(0);
-  fill(100);
-  line(200, 0, width, height);
-  textSize(24);
-  text("\"I have a quote that I'm going to use\"", 40, 250);
-  textSize(20);
-  fill(200);
-
-text("-Kanika", 350, 290);
-
 }
 
 function draw() {
